23b6d4bc751b048e2ed4892d9d8017092e93c031
kobiowuquadri <kobiowuq@gmail.com>
2023-11-14 15:13:38 -0600

    add login logic

--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
 
 
 function Register() {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+
+  const handleSubmit = async () => {
+    // API -> fetch(), axios()
+    // // http://localhost:5000/register
+   try{
+    const response =await axios.post('http://localhost:5000/register', {
+      username, 
+      email,
+      password
+    })
+    console.log(response)
+
+    navigate('/login')
+   }
+   catch(err){
+    console.log(err)
+   }
+
+  }
   return (
     <div>
         <h1 className="text-center">Register</h1>
         <form action="">
-            <input type="text" placeholder="Username" name='username'/><br/>
-            <input type="email" placeholder="Email" name='email'/><br/>
-            <input type="password" placeholder="Password" name='password'/><br/>
-            <input type="submit" value="Submit" />
+            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} name='username'/><br/>
+            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} name='email'/><br/>
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} name='password'/><br/>
+            <input type="submit" value="Submit" onClick={handleSubmit} />
         </form>
     </div>
   )
